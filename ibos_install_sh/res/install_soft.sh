2b59f6a1c5b8163119160bf1124b055c8c29152a
SeekArt <studio8@sohu.com>
2016-08-10 14:10:19 +0800

    下载时指定文件名，以避免一些环境下载没有后缀名的问题

--- ibos_install_sh/res/install_soft.sh
+++ ibos_install_sh/res/install_soft.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #IBOS
-if [ ! -f master.zip ];then
-  wget https://github.com/SeekArt/IBOS/archive/master.zip
+if [ ! -f iboscode.zip ];then
+  wget -O iboscode.zip https://github.com/SeekArt/IBOS/archive/master.zip
 fi
 rm -rf IBOS-master
-unzip master.zip
+unzip iboscode.zip
 mv IBOS-master/* /ibos/www/
 chmod -R 777 /ibos/www/system/config
 chmod -R 777 /ibos/www/data
--- install_ibos.sh
+++ install_ibos.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #IBOS
-wget https://github.com/SeekArt/ibos-linux-install/archive/master.zip
+wget -O iboslinux.zip https://github.com/SeekArt/ibos-linux-install/archive/master.zip
 rm -rf ibos-linux-install-master
-unzip master.zip
-rm -rf master.zip
+unzip iboslinux.zip
+rm -rf iboslinux.zip
 chmod -R 777 ./ibos-linux-install-master/
 cd ./ibos-linux-install-master/ibos_install_sh/
 ./install.sh
